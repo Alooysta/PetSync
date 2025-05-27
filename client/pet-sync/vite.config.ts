5cd3daf5924309f9fef8a2e3d10a4f54daf71616
Guilherme Santos <guilhermessantos2011@gmail.com>
2025-05-27 17:32:09 -0300

    teste

--- client/pet-sync/vite.config.ts
+++ client/pet-sync/vite.config.ts
@@ -3,12 +3,17 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    emptyOutDir: true,
   },
 });
